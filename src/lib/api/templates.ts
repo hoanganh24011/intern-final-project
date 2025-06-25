e59cd4a1fb18aeea552e6e41d4c0170d450f6665
Sang <dntsang2004@gmail.com>
2025-06-25 11:14:49 +0700

    refactor(templates): review source

--- src/lib/api/templates.ts
+++ src/lib/api/templates.ts
@@ -1,10 +1,13 @@
 import { apiGet } from "@utils/config-api";
 import { ApiResponse, Request } from "@type/api.type";
-import { Template, TemplateQueryParams } from "@type/api/template.type";
+import { Template, TemplatesQueryParams } from "@type/api/template.type";
 
-export const apiGetTemplates = async (params: TemplateQueryParams) => {
+const DOMAIN = "https://eb4e-2405-4802-9407-89d0-9ece-9dcd-e668-f33a.ngrok-free.app/api";
+const PATH = "/sns/templates";
+
+export const apiGetTemplates = async (params: TemplatesQueryParams) => {
 	const request: Request = {
-		url: "/templates",
+		url: `${DOMAIN}${PATH}`,
 		token: "",
 		params,
 	};
